dab8777ae758f6712396bf9d17a874b1d5953c80
Monserrat <monserrat.jimenez2@gmail.com>
2022-09-02 10:47:32 -0500

    Add Web API to Web Application

---
+++ WebApplicationWithWebApiService/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+using WebApplicationWithWebApiService.App_Start;
+
+namespace WebApplicationWithWebApiService
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
