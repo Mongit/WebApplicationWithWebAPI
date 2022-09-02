1acc0ca323f65d32c5dc1406cc59a3916c367e28
Monserrat <monserrat.jimenez2@gmail.com>
2022-09-02 10:52:14 -0500

    First Model

---
+++ WebApplicationWithWebApiService/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplicationWithWebApiService.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Contact()
+        {
+            Id = -1;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+    }
+}
