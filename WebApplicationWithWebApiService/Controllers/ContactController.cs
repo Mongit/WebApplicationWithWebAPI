eb4ea55a1be566622cedc5c41387d1117090f68e
Monserrat <monserrat.jimenez2@gmail.com>
2022-09-02 11:07:21 -0500

    First Controller

---
+++ WebApplicationWithWebApiService/Controllers/ContactController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplicationWithWebApiService.Models;
+
+namespace WebApplicationWithWebApiService.Controllers
+{
+    public class ContactController : ApiController
+    {
+        Contact[] contacts = new Contact[]
+        {
+            new Contact { Id = 1, FirstName = "Peter", LastName = "Parker" },
+            new Contact { Id = 2, FirstName = "Bruce", LastName = "Wayne" },
+            new Contact { Id = 3, FirstName = "Bruce", LastName = "Banne" },
+        };
+
+        // GET: api/Contact
+        public IEnumerable<Contact> Get()
+        {
+            return contacts;
+        }
+
+        // GET: api/Contact/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Contact
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Contact/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Contact/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
