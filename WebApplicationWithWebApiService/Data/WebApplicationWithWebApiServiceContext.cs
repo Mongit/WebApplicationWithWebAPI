5cf9a0e9660d964e6070c8af4aaf859cebdfe1a5
Monserrat <monserrat.jimenez2@gmail.com>
2022-09-05 12:27:38 -0500

    Entity Framework Code Fisrt Migration

---
+++ WebApplicationWithWebApiService/Data/WebApplicationWithWebApiServiceContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplicationWithWebApiService.Models;
+
+namespace WebApplicationWithWebApiService.Data
+{
+    public class WebApplicationWithWebApiServiceContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public WebApplicationWithWebApiServiceContext() : base("name=WebApplicationWithWebApiServiceContext")
+        {
+        }
+
+        object placeHolderVariable;
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ WebApplicationWithWebApiService/Migrations/Configuration.cs
@@ -0,0 +1,30 @@
+﻿namespace WebApplicationWithWebApiService.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using WebApplicationWithWebApiService.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplicationWithWebApiService.Data.WebApplicationWithWebApiServiceContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WebApplicationWithWebApiService.Data.WebApplicationWithWebApiServiceContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+            context.Contacts.AddOrUpdate( new Contact[] {
+                new Contact { Id = 1, FirstName = "Andrew", LastName = "Peters" },
+                new Contact { Id = 2, FirstName = "Brice", LastName = "Lambson" },
+                new Contact { Id = 3, FirstName = "Rowan", LastName = "Miller" }
+            });
+        }
+    }
+}
